5f05c03578673f3076ba09ebd1b9356d675a062d
Adelmo DIas <dev1@mzgroup.com>
2019-06-17 20:31:52 -0300

    Enabling the Cam with MediaDevices.getUserMedia()

--- src/controller/WhatAppController.js
+++ src/controller/WhatAppController.js
@@ -160,6 +160,8 @@ class WhatAppController {
       this.el.panelCamera.css({
         height: "calc(100% - 120px)"
       });
+
+      this._camera = new CameraController(this.el.videoCamera);
     });
 
     this.el.btnClosePanelCamera.on("click", e => {
